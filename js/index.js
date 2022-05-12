5ed97b1b7e3f3af70568b4df58cab519e31967a1
SShehan <sachinshehan20@gmail.com>
2022-05-13 03:48:05 +0530

    slider added
    without arrows and auto changing

--- js/index.js
+++ js/index.js
@@ -1,72 +1,13 @@
-const slider = document.querySelector(".slider");
-const nextbtn = document.querySelector(".next-btn");
-const prevbtn = document.querySelector(".prev-btn");
-const slides = document.querySelectorAll(".slide");
-const slideIcons = document.querySelector(".slide-icon");
-const numberofSlides = slides.length;
-var slideNumber = 0;
-
-nextbtn.addEventListener("click", () => {
-    slides.forEach((slide) => {
-        slide.classList.remove("active");
-    });
-    slideIcons.forEach((slideIcon) => {
-        slideIcon.classList.remove("active");
-    });
-
-    slideNumber++;
-
-    if (slideNumber > (numberofSlides - 1)) {
-        slideNumber = 0;
-    }
-
-    slides[slideNumber].classList.add("active");
-    slideIcons[slideNumber].classList.add("active");
-});
-
-prevbtn.addEventListener("click", () => {
-    slides.forEach((slide) => {
-        slide.classList.remove("active");
-    });
-    slideIcons.forEach((slideIcon) => {
-        slideIcon.classList.remove("active");
-    });
-
-    slideNumber--;
-
-    if (slideNumber > 0) {
-        slideNumber = numberofSlides - 1;
-    }
-
-    slides[slideNumber].classList.add("active");
-    slideIcons[slideNumber].classList.add("active");
-});
-
-var playslider;
-var repeater = () => {
-    playslider = setInterval(function () {
-        slides.forEach((slide) => {
-            slide.classList.remove("active");
-        });
-        slideIcons.forEach((slideIcon) => {
-            slideIcon.classList.remove("active");
-        });
-
-        slideNumber++;
-
-        if (slideNumber > (numberofSlides - 1)) {
-            slideNumber = 0;
-        }
-
-        slides[slideNumber].classList.add("active");
-        slideIcons[slideNumber].classList.add("active");
-    }, 4000);
-}
-repeater();
-
-slider.addEventListener("mouseover", () => {
-    clearInterval(playslider);
-});
-slider.addEventListener("mouseout", () => {
-    repeater();
-});
+var indexValue = 0;
+         function slideShow(){
+           setTimeout(slideShow, 2500);
+           var x;
+           const img = document.querySelectorAll("img");
+           for(x = 0; x < img.length; x++){
+             img[x].style.display = "none";
+           }
+           indexValue++;
+           if(indexValue > img.length){indexValue = 1}
+           img[indexValue -1].style.display = "block";
+         }
+         slideShow();
